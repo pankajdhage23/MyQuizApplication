766b902a0af17a9f99eb4b80cb42c231de717cec
dell <pankajdhage>
2022-09-11 20:27:25 +0530

    MyQuizApplication

---
+++ app/src/main/java/com/example/myquizapplication/QuestionAnswer.java
@@ -0,0 +1,26 @@
+package com.example.myquizapplication;
+
+public class QuestionAnswer {
+
+    public static String question [] ={
+            "The metal whose salts are sensitive to light is?",
+            "Volcanic eruption do not occur in the?",
+            "At room temperature, which is the only metal that is in liquid form?",
+            "For galvanizing iron which of the following metals is used?"
+    };
+
+    public static String choices[][] = {
+            {"Zinc","Silver","Copper","Aluminum"},
+            {"Baltic sea","Black sea","Caribbean sea","Caspian sea"},
+            {"Iron","Aluminum","Silver","Mercury"},
+            {"Lead","Copper","Aluminum","Zinc"}
+    };
+
+    public static String correctAnswers[] = {
+            "Silver",
+            "Baltic sea",
+            "Silver",
+            "Zinc"
+    };
+    
+}
